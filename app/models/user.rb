80d3bf988ead353c33659279043c6eaf3f5e7a16
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-17 15:10:20 -0400

    Add migration for email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+/.]a-z]+\z/i
     validates :email, prescence: true, length: { maximum: 255 },
                                         format: { with: VALID_EMAIL_REGEX },
-                                        uniqueness: true
+                                        uniqueness: {case_sensitive: false }
     has_secure_password
+    validates :password, prescence: true, length { minimum: 6}
 end
