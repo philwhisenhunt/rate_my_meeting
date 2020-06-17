465dcce55f81a92b5031cab913719323b2faff59
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-17 14:54:21 -0400

    Add more validations, prepare for encrypted password stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     validates :name, prescence: true, length: { maximum: 50 }
-    validates :email, prescence: true, length: { maximum: 255 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+/.]a-z]+\z/i
+    validates :email, prescence: true, length: { maximum: 255 },
+                                        format: { with: VALID_EMAIL_REGEX },
+                                        uniqueness: true
+    has_secure_password
 end
