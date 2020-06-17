cc3b0a7b1c3afc5e7e129e5c67f7b8ee98e0d75b
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-17 16:11:39 -0400

    Login page up

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
 
 end
