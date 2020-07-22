c494fb91d2fb2588fee0a311df5b2945e4782b98
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-22 10:21:05 -0400

    Begin again with a sparse user file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
 class User < ApplicationRecord
-    has_many :ratings, dependent:
-    before_save { self.email = email.downcase }
-    validates :name, prescence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+/.]a-z]+\z/i
-    validates :email, prescence: true, length: { maximum: 255 },
-                                        format: { with: VALID_EMAIL_REGEX },
-                                        uniqueness: {case_sensitive: false }
-    has_secure_password
-    validates :password, prescence: true, length { minimum: 6}
+
 end
