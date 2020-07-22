3c73028a03f3d299c3799ad1d95cc8c8d49d782a
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-22 12:38:02 -0400

    Trying to get user to accept a login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :ratings, dependent:
+    # has_many :ratings
     before_save { self.email = email.downcase }
     validates :name, prescence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+/.]a-z]+\z/i
