40a17b5338b1d770dd01c57456fa29825d880c1f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-22 13:07:29 -0400

    Starting over with sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      reset session 
-      log_in user
-      redirect_to user
+      # Log the user in and redirect to the user's show page.
     else
-      #show me an error
-      flash.now[:danger] = "Not the right combo of email and password"
+      # Create an error message.
       render 'new'
     end
   end
