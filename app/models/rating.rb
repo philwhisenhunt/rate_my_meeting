ff2e167c93b87cbae3f6eeb4c7421519f4d0b672
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-22 14:51:06 -0400

    Removing scaffold for rating

--- app/models/rating.rb
+++
@@ -1,7 +0,0 @@
-class Rating < ApplicationRecord
-    
-    belongs_to :user
-    validates :user_id, presence: true
-    validates :rating, presence: true, numericality: {only_integer: true}
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :ratings
   resources :users
   get 'sessions/new'
   get 'users/new'
