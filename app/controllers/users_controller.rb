0370a2ba4091d0cacb26bd899421f52c4faaadfd
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-22 09:34:16 -0400

    Display users (discovered we need to go back a step)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def index
+    @user = User.find(params[:id])
+    @ratings = @user.ratings.paginate(page: params[:page])  end
 end
