804523e95aa818956490d02db0ea998ade1d4616
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-22 14:08:28 -0400

    Adding in sessions resets and logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      login_in user
+      redirect_to user
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
