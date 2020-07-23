8a2546c68e695770175fc023134f03cb1b9d83c5
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-23 08:35:30 -0400

    Add descending order instruction

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ApplicationRecord
-  belongs_to :user
-  validates :user_id, presence: true
-  validates :content, presence: true
+    belongs_to :user
+    default_scope -> { order(created_at: :desc) }
+    validates :user_id, presence: true
+    validates :content, presence: true
 
 end
