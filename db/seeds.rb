0b2d71b67482216bb18593d78913cebcc5d541fe
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-23 09:50:15 -0400

    Begin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Generate ratings for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+    content = rand(11)
+    users.each { |user| user.ratings.create!(content: content) }
+end
