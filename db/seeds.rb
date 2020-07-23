f03f4236f2121ccc6be99e6dbf36907ad8baa0a9
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-23 10:19:38 -0400

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,19 @@
 # Create a main sample user.
-User.create!(name: "Example User",
-            email: "example@example.com",
-            password: "foobar",
-            password_confirmation: "foobar")
+User.create!(name:  "Example User",
+             email: "example@example.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
 
-#Generate a bunch of additional users.
+# Generate a bunch of additional users.
 99.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
-    password = "password"
-    User.create!(name: name,
-                email: email,
-                password: password,
-                password_confirmation: password)
+  name  = Faker::Name.name
+  email = "example-#{n+1}@example.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
 end
-
 # Generate ratings for a subset of users.
 users = User.order(:created_at).take(6)
 50.times do
