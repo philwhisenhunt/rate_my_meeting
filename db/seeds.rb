6f0252f954a98a3c7040c8c4a054ab2feb53721b
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-23 09:56:52 -0400

    Make users too in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,17 @@ User.create!(name: "Example User",
             password: "foobar",
             password_confirmation: "foobar")
 
+#Generate a bunch of additional users.
+99.time do |n|
+    name = Fake::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password)
+end
+
 # Generate ratings for a subset of users.
 users = User.order(:created_at).take(6)
 50.times do
