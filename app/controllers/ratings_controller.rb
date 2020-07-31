5eb1df8030ee0474e81e49d0625ff668c066dafd
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-30 22:00:45 -0400

    writing out the steps for the rating average

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,10 @@
 class RatingsController < ApplicationController
+
+    def average
+        #Find the days date
+        #Find all ratings for that date
+        #Add all the ratings together
+        #Divide by the number of ratings given
+        #Return the average number
+    end
 end
