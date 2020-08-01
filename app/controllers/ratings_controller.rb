d69d12a4007c654acdd666df0005c7df1aa3999a
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-31 23:05:04 -0400

    Working on ability to save ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,9 +2,16 @@ class RatingsController < ApplicationController
 
     def average
         #Find the days date
+        
         #Find all ratings for that date
         #Add all the ratings together
         #Divide by the number of ratings given
         #Return the average number
+    end
+
+    def save
+        #Accept the input of the rating
+        #Save it to the date of the current date
+        day = Time.day
     end
 end
