4cca0e19ad978846f7feccdda9a0394b9f7484cc
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-08-06 08:37:07 -0400

    Tried making belongs to user optional, but still not seeding

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, class_name: "User", optional: true
     default_scope -> { order(created_at: :desc) }
     validates :user_id, presence: true
     validates :content, presence: true
