9bfeb080050e7568653e3fe8b1c66a25c790a0ff
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-08-26 09:58:41 -0400

    Attempts at getting more unique numbers for db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,14 @@ end
 # Generate ratings for a subset of users.
 
 users = User.order(:created_at).take(6)
+# srand(time(0))
 50.times do |i|
-  rating = rand(11)
+  # rating = rand(11)
   meeting_date = (Date.today - i).to_s
   # meeting_date = "2020-06-18"
-  puts meeting_date
-  users.each { |user| user.ratings.create!(rating: rating,
+  # puts meeting_date
+  # puts rand(1...11)
+  users.each { |user| user.ratings.create!(rating: rand(1...11),
                                             meeting_date: meeting_date) }
 
 end
