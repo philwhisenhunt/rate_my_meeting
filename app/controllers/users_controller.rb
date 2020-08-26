e5cec2f008de7a3d09362400c3f14ac6c1323639
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-08-26 14:50:42 -0400

    Building out ability to add a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
-
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+  
   def index
   
     @users = User.paginate(page: params[:page])
@@ -17,5 +28,11 @@ class UsersController < ApplicationController
 
     # byebug
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
   end
 end
