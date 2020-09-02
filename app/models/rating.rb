4dbc32a055dc75550e2da2cc88acfd5dca208173
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-09-02 15:57:33 -0400

    Made a class method so that we can call it on multiple ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,32 +4,36 @@ class Rating < ApplicationRecord
     # validates :user_id, presence: true
     # validates :content, presence: true
 
-    def average
-        # @ratings = ratings
-        # @ratings_count = @ratings.count
-        # @ratings.each do |rating|
-        #     @ratings_sum += rating.content
-        #     puts @ratings_sum
-        # end
-   
-        # @ratings_sum = @ratings.sum
-        # average = @ratings_sum/@ratings_count
-
-
-        my_array = []
-        @ratings.each do |rating|
-           my_array.push(rating.rating)
-        end
-        total = 0
-        my_array.each do |piece|
-            total = total + piece
-        end
-        if my_array.count > 0
-            @average = total / my_array.count
-        else
-            @average = "N/A"
-        end
+    def self.avg(date)
+        self.where(meeting_date: date).average(:rating)
     end
+
+    # def average(ratings)
+    #     # @ratings = ratings
+    #     # @ratings_count = @ratings.count
+    #     # @ratings.each do |rating|
+    #     #     @ratings_sum += rating.content
+    #     #     puts @ratings_sum
+    #     # end
+   
+    #     # @ratings_sum = @ratings.sum
+    #     # average = @ratings_sum/@ratings_count
+
+    #     byebug
+    #     my_array = []
+    #     ratings.each do |rating|
+    #        my_array.push(rating.rating)
+    #     end
+    #     total = 0
+    #     my_array.each do |piece|
+    #         total = total + piece
+    #     end
+    #     if my_array.count > 0
+    #         @average = total / my_array.count
+    #     else
+    #         @average = "N/A"
+    #     end
+    # end
 
 
 end
