0681b089d82580be32736be9c1c3c7b1f14fd0ea
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-10-07 09:10:06 -0400

    New migration to Add Remember Digest to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_185927) do
+ActiveRecord::Schema.define(version: 2020_10_07_130214) do
 
   create_table "ratings", force: :cascade do |t|
     t.integer "content"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_185927) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
