63664180f6604942464589f825e5b4f6198abf8e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-10-07 08:27:01 -0400

    Build out create_activation_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,3 +16,8 @@ class User < ApplicationRecord
         email.downcase!
     end
 
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token
+    end
+end
