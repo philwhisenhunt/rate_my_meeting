9f2c9dd1c2c7491daece14844d344973f919f43e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-10-19 09:05:46 -0400

    Add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Work within a successful update
+    else
+      render 'edit'
+    end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
