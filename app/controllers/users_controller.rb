0aa2014d20ebbd9e6c66f4be7f33f838b1a23d29
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-10-23 08:39:49 -0400

    Require logged in user before index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
@@ -17,9 +17,7 @@ class UsersController < ApplicationController
   end
   
   def index
-  
-    @users = User.paginate(page: params[:page])
-
+    @users = User.all
   end
 
   def show
